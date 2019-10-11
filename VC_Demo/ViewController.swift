4cbbab45806ff9f6593c45d3f3d095dec75928e1
Artem Ulko <ulkoart@gmail.com>
2019-10-11 15:53:17 +0300

    Initial Commit

---
+++ VC_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VC_Demo
+//
+//  Created by user on 11.10.2019.
+//  Copyright © 2019 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
